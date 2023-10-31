9290783770a07ea2e9631fa19ad0bd7e5913efef
kelvinthuo999 <thuokelvinngigi@gmail.com>
2023-10-31 05:27:57 -0700

    find the height of a binary tree

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -8,11 +8,16 @@
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
+	size_t left_height = 0;
+	size_t right_height = 0;
+
 	if (tree == NULL)
 		return (0);
 
-	size_t left_height = binary_tree_height(tree->left);
-	size_t right_height = binary_tree_height(tree->right);
+	if (tree->left)
+		left_height = 1 + binary_tree_height(tree->left);
+	if (tree->right)
+		right_height = 1 + binary_tree_height(tree->right);
 
-	return (1 + (left_height > right_height ? left_height : right_height));
+	return (left_height > right_height ? left_height : right_height);
 }
