b1718ffe0f73d55630ab6cd5bc16becdc8d22adb
kelvinthuo999 <thuokelvinngigi@gmail.com>
2023-11-01 01:47:51 -0700

    changed the prototype of tree balance function

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -6,30 +6,32 @@
  *
  * Return: Balance factor of the tree, or 0 if tree is NULL
  */
+static int my_binary_tree_height(const binary_tree_t *tree);
+
 int binary_tree_balance(const binary_tree_t *tree)
 {
 	if (tree == NULL)
 		return (0);
 
-	int left_height = binary_tree_height(tree->left);
-	int right_height = binary_tree_height(tree->right);
+	int left_height = my_binary_tree_height(tree->left);
+	size_t right_height = my_binary_tree_height(tree->right);
 
 	return (left_height - right_height);
 }
 
 /**
- * binary_tree_height - Measures the height of a binary tree
+ * my_binary_tree_height - Measures the height of a binary tree
  * @tree: Pointer to the root node of the tree
  *
  * Return: Height of the tree, or 0 if tree is NULL
  */
-int binary_tree_height(const binary_tree_t *tree)
+static int my_binary_tree_height(const binary_tree_t *tree)
 {
 	if (tree == NULL)
 		return (0);
 
-	int left_height = binary_tree_height(tree->left);
-	int right_height = binary_tree_height(tree->right);
+	int left_height = my_binary_tree_height(tree->left);
+	int right_height = my_binary_tree_height(tree->right);
 
 	return (1 + (left_height > right_height ? left_height : right_height));
 }
